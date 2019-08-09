30bc1ca0fa4bc2c0336b79407532822cdad176fe
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-08-09 13:41:39 +0200

    Added WebSocket support

---
+++ src/socket/socket_client_ws.js
@@ -0,0 +1,60 @@
+'use strict'
+const W3CWebSocket = require('websocket').w3cwebsocket
+
+class WebSocketClient {
+  constructor(self, host, port, protocol, options) {
+    this.self = self
+    this.host = host
+    this.port = port
+    this.protocol = protocol
+    this.options = options
+    this.client = null
+  }
+
+  async connect() {
+    const url = `${this.protocol}://${this.host}:${this.port}`
+
+    const client = new W3CWebSocket(
+      url,
+      undefined,
+      undefined,
+      undefined,
+      this.options
+    )
+
+    this.client = client
+
+    return new Promise((resolve, reject) => {
+      client.onerror = (error) => {
+        this.self.onError(error)
+      }
+
+      client.onclose = (event) => {
+        this.self.onClose(event)
+        reject(new Error(`websocket connection closed: code: [${event.code}], reason: [${event.reason}]`))
+      }
+
+      client.onmessage = (message) => {
+        this.self.onMessage(message.data)
+      }
+
+      client.onopen = () => {
+        if (client.readyState === client.OPEN) {
+          this.self.onConnect()
+          resolve()
+        }
+      }
+    })
+  }
+
+  async close() {
+    this.client.close(1000, 'close connection')
+  }
+
+  // string
+  send(data) {
+    this.client.send(data)
+  }
+}
+
+module.exports = WebSocketClient
