f7e91375b5ccc2e4500c73471a4fe0a9c919b575
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-08-15 11:44:02 +0200

    Add tests for errored request

--- test/integration_test.js
+++ test/integration_test.js
@@ -14,33 +14,54 @@ describe('ElectrumClient', async () => {
     txData = JSON.parse(await fs.readFileSync('./test/tx.json', 'utf8'))
   })
 
-  configs.forEach((config) => {
-    let client
+  describe('for all protocols', async () => {
+    configs.forEach((config) => {
+      let client
 
-    before(async () => {
-      client = new ElectrumClient(config.host, config.port, config.protocol, config.options)
+      before(async () => {
+        client = new ElectrumClient(config.host, config.port, config.protocol, config.options)
 
-      await client.connect('test_client' + config.protocol, '1.4.2')
-        .catch((err) => {
-          console.log(err)
-        })
-    })
+        await client.connect('test_client' + config.protocol, '1.4.2')
+          .catch((err) => {
+            console.log(err)
+          })
+      })
 
-    after(async () => {
-      await client.close()
-    })
+      after(async () => {
+        await client.close()
+      })
 
-    it('blockchain_transaction_get', async () => {
-      const expoectedResult = txData.hex
-      const result = await client.blockchain_transaction_get(txData.hash)
+      it('request returns result', async () => {
+        const expoectedResult = txData.hex
+        const result = await client.blockchain_transaction_get(txData.hash)
 
-      assert.equal(
-        result,
-        expoectedResult,
-        'unexpected result',
-      )
+        assert.equal(
+          result,
+          expoectedResult,
+          'unexpected result',
+        )
+      })
     })
   })
 
+  describe('when not connected', async () => {
+    before(async () => {
+      const config = configs[0]
+
+      client = new ElectrumClient(config.host, config.port, config.protocol, config.options)
+    })
+
+    it('request throws error', async () => {
+      await client.blockchain_transaction_get(txData.hash)
+        .then(
+          (value) => { // onFulfilled
+            assert.fail('not failed as expected')
+          },
+          (reason) => { // onRejected
+            assert.include(reason.toString(), `connection not established`)
+          }
+        )
+    })
+  })
   // TODO: Add tests
 })
