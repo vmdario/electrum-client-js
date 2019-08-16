6a230aad3c691f2c6e5e05534c2f756ed560792c
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-08-16 11:48:03 +0200

    Updated examples

---
+++ example/example.js
@@ -0,0 +1,25 @@
+const ElectrumClient = require('..')
+
+async function main() {
+  const client = new ElectrumClient(
+    'fortress.qtornado.com',
+    50002,
+    'ssl'
+  )
+
+  try {
+    await client.connect(
+      'electrum-client-js', // optional client name
+      '1.4.2' // optional protocol version
+    )
+
+    const header = await client.blockchain_headers_subscribe()
+    console.log('Current header:', header)
+
+    await client.close()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+main()
--- example/scripthash.js
+++ example/scripthash.js
@@ -1,18 +1,18 @@
 const ElectrumClient = require('..')
 
 const main = async () => {
-  const ecl = new ElectrumClient(50002, 'bitcoins.sk', 'tls')
+  const ecl = new ElectrumClient('fortress.qtornado.com', 50002, 'tls')
   await ecl.connect()
   try {
-    const ver = await ecl.server_version('3.0.5', '1.1')
+    const ver = await ecl.server_version('3.0.5', '1.4')
     console.log(ver)
-    const balance = await ecl.blockchainScripthash_getBalance('676ca8550e249787290b987e12cebdb2e9b26d88c003d836ffb1cb03ffcbea7c')
+    const balance = await ecl.blockchain_scripthash_getBalance('676ca8550e249787290b987e12cebdb2e9b26d88c003d836ffb1cb03ffcbea7c')
     console.log(balance)
-    const unspent = await ecl.blockchainScripthash_listunspent('676ca8550e249787290b987e12cebdb2e9b26d88c003d836ffb1cb03ffcbea7c')
+    const unspent = await ecl.blockchain_scripthash_listunspent('676ca8550e249787290b987e12cebdb2e9b26d88c003d836ffb1cb03ffcbea7c')
     console.log(unspent)
-    const history = await ecl.blockchainScripthash_getHistory('676ca8550e249787290b987e12cebdb2e9b26d88c003d836ffb1cb03ffcbea7c')
+    const history = await ecl.blockchain_scripthash_getHistory('676ca8550e249787290b987e12cebdb2e9b26d88c003d836ffb1cb03ffcbea7c')
     console.log(history)
-    const mempool = await ecl.blockchainScripthash_getMempool('676ca8550e249787290b987e12cebdb2e9b26d88c003d836ffb1cb03ffcbea7c')
+    const mempool = await ecl.blockchain_scripthash_getMempool('676ca8550e249787290b987e12cebdb2e9b26d88c003d836ffb1cb03ffcbea7c')
     console.log(mempool)
   } catch (e) {
     console.log(e)
